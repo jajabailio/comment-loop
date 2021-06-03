5e3486c47374f969df35f8fae89430d9ff36f70f
Janssen Bailio <janssenbailio@gmail.com>
2021-06-03 18:29:23 +0800

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 require('./startup/routes')(app);   //! routes
 require('./startup/db')();       //! database
 require('./startup/test');
