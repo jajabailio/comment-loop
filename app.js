023852cd548e701e0019a6fafc03ef03f1a5fc87
Janssen Bailio <janssenbailio@gmail.com>
2021-06-03 18:03:13 +0800

    added helmet and compression packages for testing environment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 require('./startup/routes')(app);   //! routes
 require('./startup/db')();       //! database
+require('./startup/test');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening to port ${PORT}...`));
