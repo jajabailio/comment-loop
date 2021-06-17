0153c832fde51e108e5ad19956ebc204318948c2
Janssen Bailio <49528068+jajabailio@users.noreply.github.com>
2021-06-17 16:39:29 +0800

    updated the reads of the list by all survey

--- controllers/responses/reads/listAllBySurvey/index.js
+++ controllers/responses/reads/listAllBySurvey/index.js
@@ -7,7 +7,7 @@ exports.surveyResponses = async (req, res) => {
     try {
 
         const fetchSurvey = await Survey.aggregate([
-            { "$match": { _id: ObjectId(req.params.survey_id) }},
+            { "$match": { _id: ObjectId(req.params.survey_id) } },
             {
                 "$lookup": {
                     "from": "questions",
@@ -16,7 +16,7 @@ exports.surveyResponses = async (req, res) => {
                     "as": "questions"
                 }
             },
-            { "$unwind": { "path": "$questions", "preserveNullAndEmptyArrays": true} },
+            { "$unwind": { "path": "$questions", "preserveNullAndEmptyArrays": true } },
             {
                 "$lookup": {
                     "from": "options",
@@ -25,27 +25,38 @@ exports.surveyResponses = async (req, res) => {
                     "as": "questions.options"
                 }
             },
-            { "$unwind": { "path": "$questions.options", "preserveNullAndEmptyArrays": true} },
+            { "$unwind": { "path": "$questions.options", "preserveNullAndEmptyArrays": true } },
             {
                 "$lookup": {
                     "from": "answers",
                     "localField": "questions.options._id",
                     "foreignField": "option_id",
-                    "as": "questions.options.answers"
+                    "as": "questions.answers"
                 },
             },
-            { "$project": {
-                "createdAt": 0, "updatedAt": 0, "isActive": 0,
-                "questions.createdAt": 0, "questions.updatedAt": 0,
-                "questions.options.createdAt": 0, "questions.options.updatedAt": 0,
-                "questions.options.answers.createdAt": 0, "questions.options.answers.updatedAt": 0, 
-                
-            }},
+            // { "$unwind": { "path": "$questions.answers", "preserveNullAndEmptyArrays": true } },
+            // {
+            //     "$lookup": {
+            //         "from": "responses",
+            //         "localField": "questions.answers.responses._id",
+            //         "foreignField": "response_id",
+            //         "as": "questions.answers.responses"
+            //     }
+            // },
+            {
+                "$project": {
+                    "createdAt": 0, "updatedAt": 0, "isActive": 0, "__v": 0,
+                    "questions.createdAt": 0, "questions.updatedAt": 0, "questions.survey_id": 0, "questions.isMain": 0, "questions.__v": 0, "questions.order_number": 0, "questions.sub_question_id": 0, "questions.question_id": 0,
+                    "questions.options.createdAt": 0, "questions.options.updatedAt": 0, "questions.options.question_id": 0, "questions.options.__v": 0, "questions.options.order_number": 0,
+                    "questions.answers.createdAt": 0, "questions.answers.updatedAt": 0, "questions.answers.question_id": 0, "questions.answers.option_id": 0, "questions.answers.__v": 0
+
+                }
+            },
         ]);
 
         res.json(fetchSurvey);
 
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.status(500).json('Internal Server Error');
     }
